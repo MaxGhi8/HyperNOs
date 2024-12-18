ad0f37abfc53580157860e1fea36c0528fdb04a8
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2024-12-18 11:47:16 +0100

    initialize argparse instead of sys

--- neural_operators/recover_model.py
+++ neural_operators/recover_model.py
@@ -33,7 +33,7 @@ from jaxtyping import Float, jaxtyped
 from beartype import beartype
 import numpy as np
 import time
-import sys
+import argparse
 import json
 import matplotlib.pyplot as plt
 import os
@@ -52,30 +52,80 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Device: ", device)
 # torch.set_default_dtype(torch.float32) # default tensor dtype
 
-mode_str = "best"  # test base hyperparameters, can be "default" or "best"
+mode_str = "default"  # test base hyperparameters, can be "default" or "best"
+
 
 #########################################
 # Choose the example to run
 #########################################
-if len(sys.argv) < 3:
-    raise ValueError("The user must choose the example and the model to run")
-elif len(sys.argv) == 3:
-    which_example = sys.argv[1]
-    arc = sys.argv[2].upper()
-    exp_norm = "L1"  # default value
-    in_dist = True  # default value
-elif len(sys.argv) == 4:
-    which_example = sys.argv[1]
-    arc = sys.argv[2].upper()
-    exp_norm = sys.argv[3].upper()
-    in_dist = True  # default value
-elif len(sys.argv) == 5:
-    which_example = sys.argv[1]
-    arc = sys.argv[2].upper()
-    exp_norm = sys.argv[3].upper()
-    in_dist = sys.argv[4]
-else:
-    raise ValueError("The user must choose the example to run")
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Run a specific example with the desired model and training configuration."
+    )
+
+    parser.add_argument(
+        "example",
+        type=str,
+        choices=[
+            "poisson",
+            "wave_0_5",
+            "cont_tran",
+            "disc_tran",
+            "allen",
+            "shear_layer",
+            "airfoil",
+            "darcy",
+            "burgers_zongyi",
+            "darcy_zongyi",
+            "fhn",
+            "fhn_long",
+            "hh",
+            "crosstruss",
+        ],
+        help="Select the example to run.",
+    )
+    parser.add_argument(
+        "architecture",
+        type=str,
+        choices=["fno", "cno"],
+        help="Select the architecture to use.",
+    )
+    parser.add_argument(
+        "loss_function",
+        type=str,
+        choices=["l1", "l2", "h1", "l1_smooth"],
+        help="Select the relative loss function to use during the training process.",
+    )
+    parser.add_argument(
+        "mode",
+        type=str,
+        choices=["best", "default"],
+        help="Select the hyper-params to use for define the architecture and the training, we have implemented the 'best' and 'default' options.",
+    )
+    parser.add_argument(
+        "--in_dist",
+        type=bool,
+        default=True,
+        help="For the datasets that are supported you can select if the test set is in-distribution or out-of-distribution.",
+    )
+
+    args = parser.parse_args()
+
+    return {
+        "example": args.example.lower(),
+        "architecture": args.architecture.upper(),
+        "loss_function": args.loss_function.upper(),
+        "mode": args.mode.lower(),
+        "in_dist": args.in_dist,
+    }
+
+
+config = parse_arguments()
+which_example = config["example"]
+arc = config["architecture"]
+exp_norm = config["loss_function"]
+mode_str = config["mode"]
+in_dist = config["in_dist"]
 
 Norm_dict = {"L1": 0, "L2": 1, "H1": 2, "L1_SMOOTH": 3, "MSE": 4}
 
