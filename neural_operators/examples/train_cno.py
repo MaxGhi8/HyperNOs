29c3adb74fd1e971c2adb45b949a52aa86957344
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-01-04 08:40:57 +0100

    add default fno test

--- neural_operators/examples/train_cno.py
+++ neural_operators/examples/train_cno.py
@@ -96,4 +96,4 @@ def train_cno(which_example: str, mode_hyperparams: str, loss_fn_str: str):
 
 
 if __name__ == "__main__":
-    train_cno("darcy", "default", "L2")
+    train_cno("poisson", "default", "L2")
