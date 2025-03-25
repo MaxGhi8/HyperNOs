a466a9d1cf59c0b475b33c5af0b58e6a2def9c58
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-03-25 15:59:47 +0100

    initialize resnet

---
+++ neural_operators/ResNet/ResidualNetwork.py
@@ -0,0 +1,143 @@
+"""
+This file contains all the core architectures and modules of the Fourier Neural Operator (FNO) for 1D case.
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from beartype import beartype
+from jaxtyping import Float, jaxtyped
+from torch import Tensor
+
+torch.set_default_dtype(torch.float32)  # default tensor dtype
+
+
+#########################################
+# activation function
+#########################################
+def activation_fun(activation_str):
+    """
+    Activation function to be used within the network.
+    The function is the same throughout the network.
+    """
+    if activation_str == "relu":
+        return nn.ReLU()
+    elif activation_str == "gelu":
+        return nn.GELU()
+    elif activation_str == "tanh":
+        return nn.Tanh()
+    elif activation_str == "leaky_relu":
+        return nn.LeakyReLU()
+    elif activation_str == "sigmoid":
+        return nn.Sigmoid()
+    else:
+        raise ValueError("Not implemented activation function")
+
+
+@jaxtyped(typechecker=beartype)
+def centered_softmax(x: Float[Tensor, "n_samples n"]) -> Float[Tensor, "n_samples n"]:
+    """
+    Centered softmax function to be used within the network.
+    """
+    return F.softmax(x, dim=1) - 1 / x.shape[1]
+
+
+@jaxtyped(typechecker=beartype)
+def zero_mean_imposition(
+    x: Float[Tensor, "n_samples n"],
+) -> Float[Tensor, "n_samples n"]:
+    """
+    Take a vector and impose the zero mean constraint.
+    """
+    return x - x.mean(dim=1, keepdim=True)
+
+
+#########################################
+# Residual Block
+#########################################
+class ResidualBlock(nn.Module):
+    """
+    Residual block for the ResNet
+    """
+
+    def __init__(
+        self,
+        hidden_channels: list[int],
+        activation_str: str,
+    ) -> None:
+        super(ResidualBlock, self).__init__()
+        self.hidden_channels = hidden_channels
+
+        modules = []
+        for i in range(len(self.hidden_channels) - 1):
+            # Affine layer
+            modules.append(
+                nn.Linear(self.hidden_channels[i], self.hidden_channels[i + 1])
+            )
+
+            # Activation function
+            modules.append(activation_fun(activation_str))
+
+        self.res_block = nn.Sequential(*modules)
+
+    @jaxtyped(typechecker=beartype)
+    def forward(
+        self, x: Float[Tensor, "n_samples {self.hidden_channels[0]}"]
+    ) -> Float[Tensor, "n_samples {self.hidden_channels[-1]}"]:
+        return self.res_block(x)
+
+
+#########################################
+# Residual Network
+#########################################
+class ResidualNetwork(nn.Module):
+    """
+    Residual Network for the Fourier Neural Operator
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        hidden_channels: list[int],
+        activation_str: str,
+        n_blocks: int,
+        zero_mean: bool = False,
+    ) -> None:
+        super(ResidualNetwork, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        assert (
+            hidden_channels[-1] == hidden_channels[0]
+        ), "The input and output dimensions must be the same for being concatenated"
+        assert n_blocks >= 0, "Number of layers must be greater or equal to 0"
+
+        self.input_layer = nn.Sequential(
+            nn.Linear(in_channels, hidden_channels[0]), activation_fun(activation_str)
+        )
+
+        self.residual_blocks = nn.ModuleList(
+            [ResidualBlock(hidden_channels, activation_str) for _ in range(n_blocks)]
+        )
+        
+        self.output_layer = nn.Sequential(
+            nn.Linear(hidden_channels[-1], out_channels), activation_fun(activation_str)
+        )
+
+        self.post_processing = zero_mean_imposition if zero_mean else nn.Identity
+
+    @jaxtyped(typechecker=beartype)
+    def forward(
+        self, x: Float[Tensor, "n_samples {self.in_channels}"]
+    ) -> Float[Tensor, "n_samples {self.out_channels}"]:
+
+        x = self.input_layer(x)
+
+        for res_block in self.residual_blocks:
+            x = x + res_block(x)
+
+        x = self.output_layer(x)
+
+        return zero_mean_imposition(x)
---
+++ neural_operators/pytest/test_residualnetwork.py
@@ -0,0 +1,57 @@
+import sys
+
+sys.path.append("..")
+
+import torch
+from ResNet.ResidualNetwork import (
+    ResidualNetwork,
+    centered_softmax,
+    zero_mean_imposition,
+)
+
+torch.set_default_dtype(torch.float32)
+
+
+def test_centered_softmax():
+    x = torch.rand(32, 10)
+    x = centered_softmax(x)
+    assert x.shape == (32, 10)
+    assert torch.allclose(x.sum(dim=1), torch.zeros(32), atol=1e-6)
+
+
+def test_zero_mean_imposition():
+    x = torch.rand(32, 10)
+    x = zero_mean_imposition(x)
+    assert x.shape == (32, 10)
+    assert torch.allclose(x.sum(dim=1), torch.zeros(32), atol=1e-6)
+
+
+def test_residualnetwork():
+    in_channels = 7
+    out_channels = 3
+    hidden_channels = [5, 5, 5]
+    n_blocks = 2
+    activation_str = "relu"
+    model = ResidualNetwork(
+        in_channels, out_channels, hidden_channels, activation_str, n_blocks
+    )
+    batch_size = 32
+    input = torch.rand(batch_size, in_channels)
+    output = model.forward(input)
+    # test if the model runs
+    assert output.shape[-1] == out_channels
+    assert output.shape[0] == batch_size
+
+    # test zero mean
+    model = ResidualNetwork(
+        in_channels,
+        out_channels,
+        hidden_channels,
+        activation_str,
+        n_blocks,
+        zero_mean=True,
+    )
+    output = model.forward(input)
+    assert output.shape[-1] == out_channels
+    assert output.shape[0] == batch_size
+    assert torch.allclose(output.sum(dim=1), torch.zeros(batch_size), atol=1e-6)
