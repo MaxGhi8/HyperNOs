db090583d86048acce3b13110923bcabf3f70ec0
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-04-22 12:12:09 +0200

    add test for json

--- neural_operators/pytest/test_bampno.py
+++ neural_operators/pytest/test_bampno.py
@@ -4,13 +4,13 @@ sys.path.append("..")
 
 import torch
 from BAMPNO import BAMPNO
+from utilities import initialize_hyperparameters
 
 torch.set_default_dtype(torch.float32)
 
 
 def test_BAMPNO():
 
-    # Select available device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     BC = {0: (1, 2, 3), 1: (1, 4), 2: (2, 3, 4)}
@@ -36,6 +36,47 @@ def test_BAMPNO():
         FFTnorm=None,
         device=device,
         retrain_seed=4,
+    )
+
+    dummy_input = torch.randn(32, 3, 60, 60, 1).to(device)
+    output = model(dummy_input)
+    assert output.shape[:-1] == dummy_input.shape[:-1]
+    assert output.shape[-1] == 1
+
+
+def test_BAMPNO_from_json():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Load the default hyperparameters for the FNO model
+    hyperparams_train, hyperparams_arc = initialize_hyperparameters(
+        "BAMPNO", "Lshape_domain", "default"
+    )
+
+    config = {
+        **hyperparams_train,
+        **hyperparams_arc,
+    }
+
+    model = BAMPNO(
+        problem_dim=config["problem_dim"],
+        n_patch=config["n_patch"],
+        continuity_condition=config["continuity_condition"],
+        n_pts=config["n_pts"],
+        grid_filename=config["grid_filename"],
+        in_dim=config["in_dim"],
+        d_v=config["d_v"],
+        out_dim=config["out_dim"],
+        L=config["L"],
+        modes=config["modes"],
+        fun_act=config["fun_act"],
+        weights_norm=config["weights_norm"],
+        zero_BC={int(k): v for k, v in config["zero_BC"].items()},
+        arc=config["arc"],
+        RNN=config["RNN"],
+        same_params=config["same_params"],
+        FFTnorm=config["FFTnorm"],
+        device=device,
+        retrain_seed=config["retrain_seed"],
     )
 
     dummy_input = torch.randn(32, 3, 60, 60, 1).to(device)
